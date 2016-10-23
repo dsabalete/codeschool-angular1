3a97c56c45cb72701533d70f23d02b73c93f8866
David Sabalete <dsabalete@gmail.com>
2016-10-23 22:41:36 +0200

    Lesson Built-in directives

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gemStore.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gemStore
+ */
+angular.module('gemStore')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
